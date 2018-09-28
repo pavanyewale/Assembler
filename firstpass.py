693c8e121a49e4e418c82406fcbb0a744bc2c3f9
pavanyewale <pavanyewale1996@gmail.com>
2018-09-29 02:00:44 +0530

    literal table added

--- firstpass.py
+++ firstpass.py
@@ -1,29 +1,60 @@
+import sys
+from print_table import print_table
+
 register=["eax","ecx","edx","ebx","esp","ebp","esi","edi"]
 data=[("db",1),("dd",4),("resb",1),("resd",4)]
+lines=[]
+sym_struct=["Name","Size","No.elements","sym/label","declare/used","Address","Value","Hex"]
+
+def issymbol(sym):
+    for i in sym:
+        if(ord('a')>ord(i)) or ord(i)>ord('z'):
+            return False
+    return True
+
+def isdigit(strr):
+    for i in strr:
+        if(ord(i)<ord('0')) or (ord(i)>ord('9')):
+            return False
+    return True
+
+def strtohex(s):
+    lst = []
+    for ch in s:
+        hv = hex(ord(ch)).replace('0x', '')
+        if len(hv) == 1:
+            hv = '0'+hv
+        lst.append(hv)
+    return reduce(lambda x,y:x+y, lst)
+
+
+
 def indexof(inn,off):
     for i in range(len(inn)):
         if(off==inn[i]):
             return i
     return -1
-firstpass=[]
 def searchdata(st):
 	for j in data:
 		if(j[0]==st):
 			return j[1]
 	return None 
 	
-def makeEntry(name,dsize,n,typ,dou,address,value):
-	return [name,dsize,n,typ,dou,address,value]
-errortable=[]
-table=[]
+def makeEntry(name,dsize,n,typ,dou,address,value): 
+	return [name,dsize,n,typ,dou,address,value,"Not done"]
+errorsymbol_tbl=[]
+intermediate_code=[]   #output of first_pass
+symbol_tbl=[]      #["Name","Size","No.elements","sym/label","declare/used","Address","Value","Hex"]
+literal_tbl=[]  #structure ["Literal","Ascii"] 
 addr=0
+
 def processdata(l):
-	global table,addr,data,firstpass
+	global symbol_tbl,addr,data,intermediate_code
 	print("processing data")
 	for k in range(l+1,len(lines)):
 		if(lines[k][0]=="section"):
 			return k
-                firstpass.append(lines[k])
+                intermediate_code.append(lines[k])
 		if(lines[k][1]=="db"):
 			d=lines[k][2]
 			s=len(d)
@@ -31,74 +62,89 @@ def processdata(l):
 			d=lines[k][2].split(',')
 			s=len(d)
 		ds=searchdata(lines[k][1])
-		table.append(makeEntry(lines[k][0],ds,s,"S","D",addr,d))
+		symbol_tbl.append(makeEntry(lines[k][0],ds,s,"S","D",addr,d))
 		addr+=s*ds
 	return k
 		
 			
 def processbss(l):
-	global table,addr,data,firstpass
+	global symbol_tbl,addr,data,intermediate_code
 	for k in range(l+1,len(lines)):
 		if(lines[k][0]=="section"):
 			return k
-                firstpass.append(lines[k])
+                intermediate_code.append(lines[k])
 		d=None
 		s=int(lines[k][2])
 		ds=searchdata(lines[k][1])
-		table.append(makeEntry(lines[k][0],searchdata(lines[k][1]),s,"S","D",addr,d))
+		symbol_tbl.append(makeEntry(lines[k][0],searchdata(lines[k][1]),s,"S","D",addr,d))
 		addr+=s*ds
 	return k
-def updatetable(name,dou,s):
-	global table,addr,data
+
+def updatesymbol_tbl(name,dou,s):
+	global symbol_tbl,addr,data
 	f=1
 	e=[name,None,None,"L",dou,s,None]
-	for k in range(len(table)):
-		if(table[k][0]==name):
+        for k in range(len(symbol_tbl)):
+		if(symbol_tbl[k][0]==name):
 			f=0
 			if(dou=="D"):
-				table[k]=e
+				symbol_tbl[k]=e
 	if(f):
-		table.append(e)
+		symbol_tbl.append(e)
 
             #if operand is 32 bit register then there is 00 and then register no
             #if operand is 16 bit register then there is 01 and then register no
-            #if operand is symbol then there is 10 and then index of symbol table entry of operand
+            #if operand is symbol then there is 10 and then index of symbol symbol_tbl entry of operand
+
 def processoprands1(ops):
+        if(isdigit(ops)):
+            #handle number literal here
+            literal_tbl.append((hex(int(ops))))
+            return "lit#"+str(len(literal_tbl))
+            return ops 
+        if(ops[0]=='"') or (ops[0]=="'"):
+                #handle string literal here
+                literal_tbl.append(strtohex((ops[:len(ops)-1])[1:]))
+                return "lit#"+str(len(literal_tbl))
+                return ops
         op1=ops.split('+')
         if(len(op1)>1):
             op11=[]
             for i in op1:
-                processoprands(i)
+                op11.append(processoprands(i))
             return joinwith(op11,'+')
         else:
             op1=ops.split('*')
             op11=[]
             if(len(op1)>1):
                 for i in op1:
-                    processoperands(i)
+                    op11.append(processoprands(i))
                 return joinwith(op11,"*")
             else: 
                 return processoprands(ops)
 def searchsymbol(sym):
-    global table
-    for i in range(len(table)):
-        if((table[i])[0]==sym):
+    global symbol_tbl
+    for i in range(len(symbol_tbl)):
+        if((symbol_tbl[i])[0]==sym):
             return i
     return -1
 def processoprands(opp):
-    global register,table
+    global register,symbol_tbl
     k=indexof(register,opp)
     if(k!=-1):
-        return "00"+str(k)
+        return "reg#"+str(k)
     if(opp[:5]=="dword"):
         return "dword["+processoprands1(opp[6:len(opp)-1])+"]"
     if(opp[:4]=="byte"):
         return "byte["+processoprands(opp[5:len(opp)-1])+"]"
-    k=searchsymbol(opp)
-    if(k!=-1):
-        return "10"+str(k)
-    table.append([opp,None,None,"S","U",None,None])
-    return "10"+str(len(table)-1)
+    if(issymbol(opp)):
+        k=searchsymbol(opp)
+        if(k!=-1):
+            return "sym#"+str(k)
+        symbol_tbl.append([opp,None,None,"S","U",None,None])
+        return "sym#"+str(len(symbol_tbl)-1)
+    else:
+        return processoprands1(opp)
 
 def joinwith(li,sym):
     new=""
@@ -110,15 +156,15 @@ def joinwith(li,sym):
     return new
 
 def processtext(l):
-	global table,addr,data,firstpass,register
+	global symbol_tbl,addr,data,intermediate_code,register
 	for k in range(l+1,len(lines)):
             nl=[]
             if((lines[k][0])[len(lines[k][0])-1:]==":"):
-                firstpass.append(lines[k])
-                updatetable((lines[k][0])[:len(lines[k][0])-1],"D",k)
+                intermediate_code.append(lines[k])
+                updatesymbol_tbl((lines[k][0])[:len(lines[k][0])-1],"D",k)
 	    else:
 			if(lines[k][0] in ["jmp","jnz","jz","call"]):
-				updatetable(lines[k][1],"U",None)
+				updatesymbol_tbl(lines[k][1],"U",None)
                         if(len(lines[k])>1):
                             new=[]
                             new.append(lines[k][0])
@@ -127,34 +173,37 @@ def processtext(l):
                             for o in oprands:
                                 new1.append(processoprands(o))
                             new.append(joinwith(new1,','))
-                            firstpass.append(new)
+                            intermediate_code.append(new)
                         else:
-                            firstpass.append(lines[k])
+                            intermediate_code.append(lines[k])
 	return k
-#fname=input("enter file name")
-#print(fname)
-fname=str(input("Enter file name: "))
-f=open(fname,"r").readlines()
-lines=[[k.strip() for k in i] for i in [i.split(' ') for i in f]]
-for l in range(len(lines)):
-	if(lines[l][0]=="section"):
-                firstpass.append(lines[l])
-		if(lines[l][1]==".data"):
-			l=processdata(l)
-		else:
-			if(lines[l][1]==".bss"):
-				l=processbss(l)
-			else:
-				if(lines[l][1]==".text"):
-					l=processtext(l)	
+def first_pass(fname):
+    global lines
+    f=open(fname,"r").readlines()
+    lines=[[k.strip() for k in i] for i in [i.split(' ') for i in f]]
+    for l in range(len(lines)):
+	    if(lines[l][0]=="section"):
+                    intermediate_code.append(lines[l])
+		    if(lines[l][1]==".data"):
+			    l=processdata(l)
+		    else:
+			    if(lines[l][1]==".bss"):
+				    l=processbss(l)
+			    else:
+				    if(lines[l][1]==".text"):
+					    l=processtext(l)	
 
-print(firstpass)
-#output
-#print(table)
-f2=open(fname+".fp","w")
-for i in firstpass:
-    if(i[0]=="section")|((i[0])[len(i[0])-1:]==":"):
-        f2.write(joinwith(i," ")+"\n")
-    else:
-        f2.write("\t"+joinwith(i," ")+"\n")
+def write_intermediate(fname):
+    f2=open(fname+".i","w")
+    for i in intermediate_code:
+        if(i[0]=="section")|((i[0])[len(i[0])-1:]==":"):
+            f2.write(joinwith(i," ")+"\n")
+        else:
+            f2.write("\t"+joinwith(i," ")+"\n")
+
+
+first_pass("class1.asm")
+print_table("symbol_table",sym_struct,symbol_tbl)
+
+#**************2nd pass****************
 
